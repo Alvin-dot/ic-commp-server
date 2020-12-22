74534641bf5605aaa91e8f24bfd04acef877aec0
AlvaroJoseBaranoski <alvarojosebaranoski01@gmail.com>
2020-12-22 11:52:35 -0300

    Changes file path to server file

--- graphs.php
+++ graphs.php
@@ -21,7 +21,7 @@ if(isset($_GET['action']) && !empty($_GET['action']))
 function startup($pmu, $time_w, $sample_freq) {
 	
     // Execute the python script with the JSON data
-	$results = shell_exec("python C:\Users\alvar\Desktop\IC-COMMP\ic-commp\startup.py $pmu $time_w $sample_freq");
+	$results = shell_exec("/opt/ic-commp/bin/python3 /opt/ic-commp/ic-commp/startup.py $pmu $time_w $sample_freq");
 
 	$data_results = json_decode($results, true);
 
