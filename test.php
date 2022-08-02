863a38fffc7adc15c42a11d582a9982ec5ff7b5d
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-08-02 14:26:09 -0300

    test

--- test.php
+++ test.php
@@ -12,8 +12,8 @@ $outlier_constant = 3.5;
 $results = shell_exec("/opt/ic-commp/bin/python3 /opt/ic-commp/ic-commp/startup.py $pmu $time_w $sample_freq $segmentWindow $segmentOverlap $filter_lower $filter_higher $outlier_constant");
 // $results = shell_exec("D:\Alvaro\Faculdade\TCC\Source\ic-commp-welch-backend\\venv\Scripts\python.exe D:\Alvaro\Faculdade\TCC\Source\ic-commp-welch-backend\\startup.py $pmu $time_w $sample_freq $segmentWindow $segmentOverlap $filter_lower $filter_higher $outlier_constant $view");
 
-print_r($results);
+// print_r($results);
 
 $data_results = json_decode($results, true);
 
-// print_r(json_decode($results, true));
+print_r(json_decode($results, true));
