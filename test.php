3f61f383a987e5ae9421cbeb8ee561d23d2c4086
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-08-02 14:31:50 -0300

    test

--- test.php
+++ test.php
@@ -1,15 +1,15 @@
 <?php
 
 $pmu = "agrarias";
-$time_w = 20;
-$sample_freq = 120;
+$time_window = 20;
+$sample_frequency = 120;
 $segmentWindow = 100;
 $segmentOverlap = 50;
 $filter_lower = 0.3;
 $filter_higher = 7.0;
 $outlier_constant = 3.5;
 
-$results = shell_exec("/opt/ic-commp/bin/python3 /opt/ic-commp/ic-commp/startup.py $pmu $time_w $sample_freq $segmentWindow $segmentOverlap $filter_lower $filter_higher $outlier_constant");
+$results = shell_exec("/opt/ic-commp/bin/python3 /opt/ic-commp/ic-commp/startup.py $pmu $time_window $sample_frequency $segmentWindow $segmentOverlap $filter_lower $filter_higher $outlier_constant");
 // $results = shell_exec("D:\Alvaro\Faculdade\TCC\Source\ic-commp-welch-backend\\venv\Scripts\python.exe D:\Alvaro\Faculdade\TCC\Source\ic-commp-welch-backend\\startup.py $pmu $time_w $sample_freq $segmentWindow $segmentOverlap $filter_lower $filter_higher $outlier_constant $view");
 
 // print_r($results);
