b908e0629753753b9b06cf1823022b43f8089810
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-08-02 14:08:53 -0300

    test

--- graphs.php
+++ graphs.php
@@ -3,31 +3,33 @@
 ini_set('max_execution_time', '256'); //max_execution_time','0' <- unlimited time
 ini_set('memory_limit', '512M');
 
-if (isset($_GET['action']) && !empty($_GET['action'])) {
-	$action = $_GET['action'];
-	$pmu = (isset($_GET['pmu']) && !empty($_GET['pmu']) ? $_GET['pmu'] : "");
-	$time_window = (isset($_GET['time_w']) && !empty($_GET['time_w']) ? $_GET['time_w'] : "");
-	$sample_frequency = (isset($_GET['sample_freq']) && !empty($_GET['sample_freq']) ? $_GET['sample_freq'] : "");
-	$segment_window = $_GET['segment_window'];
-	$segment_overlap = $_GET['segment_overlap'];
-	$filter_lower = $_GET['filter_lower'];
-	$filter_higher = $_GET['filter_higher'];
-	$outlier_constant = $_GET['outlier_constant'];
-	switch ($action) {
-		case 'startup':
-			startup(
-				$pmu, 
-				$time_window, 
-				$sample_frequency, 
-				$segment_window,
-				$segment_overlap,
-				$filter_lower,
-				$filter_higher,
-				$outlier_constant,
-			);
-			break;
-	}
-}
+
+
+// if (isset($_GET['action']) && !empty($_GET['action'])) {
+// 	$action = $_GET['action'];
+// 	$pmu = (isset($_GET['pmu']) && !empty($_GET['pmu']) ? $_GET['pmu'] : "");
+// 	$time_window = (isset($_GET['time_w']) && !empty($_GET['time_w']) ? $_GET['time_w'] : "");
+// 	$sample_frequency = (isset($_GET['sample_freq']) && !empty($_GET['sample_freq']) ? $_GET['sample_freq'] : "");
+// 	$segment_window = $_GET['segment_window'];
+// 	$segment_overlap = $_GET['segment_overlap'];
+// 	$filter_lower = $_GET['filter_lower'];
+// 	$filter_higher = $_GET['filter_higher'];
+// 	$outlier_constant = $_GET['outlier_constant'];
+// 	switch ($action) {
+// 		case 'startup':
+// 			startup(
+// 				$pmu, 
+// 				$time_window, 
+// 				$sample_frequency, 
+// 				$segment_window,
+// 				$segment_overlap,
+// 				$filter_lower,
+// 				$filter_higher,
+// 				$outlier_constant,
+// 			);
+// 			break;
+// 	}
+// }
 
 // Main function, gets data from startup.py
 // TODO: Mudar o path e o interpreter do programa em python
@@ -39,5 +41,6 @@ function startup($pmu, $time_w, $sample_freq, $segment_window, $segment_overlap,
 	// $results = shell_exec("D:/Alvaro/Faculdade/TCC/Source/ic-commp-welch-backend/venv/Scripts/python.exe D:/Alvaro/Faculdade/TCC/Source/ic-commp-welch-backend//startup.py $pmu $time_w $sample_freq $segment_window $segment_overlap $filter_lower $filter_higher $outlier_constant");
 
 	// echo json_encode($results);
-	echo "hello world!";
 }
+
+echo "hello world!";
