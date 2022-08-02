084286f87cc68b213ccd044c15582f7385e87f57
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-08-02 14:25:22 -0300

    test

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-$pmu = "palotina";
+$pmu = "agrarias";
 $time_w = 20;
 $sample_freq = 120;
 $segmentWindow = 100;
@@ -8,7 +8,6 @@ $segmentOverlap = 50;
 $filter_lower = 0.3;
 $filter_higher = 7.0;
 $outlier_constant = 3.5;
-$view = "complete";
 
 $results = shell_exec("/opt/ic-commp/bin/python3 /opt/ic-commp/ic-commp/startup.py $pmu $time_w $sample_freq $segmentWindow $segmentOverlap $filter_lower $filter_higher $outlier_constant");
 // $results = shell_exec("D:\Alvaro\Faculdade\TCC\Source\ic-commp-welch-backend\\venv\Scripts\python.exe D:\Alvaro\Faculdade\TCC\Source\ic-commp-welch-backend\\startup.py $pmu $time_w $sample_freq $segmentWindow $segmentOverlap $filter_lower $filter_higher $outlier_constant $view");
